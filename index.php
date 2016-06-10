8324920d2dfe7f906ab8b370e5296398e7bb7f4a
Thai Trung Duc <dbom.1006@gmail.com>
2016-06-11 01:34:49 +0700

    php-include

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php 
-$header=file_get_contents("header.html");
-$banner=file_get_contents("banner.html");
-$content=file_get_contents("content.html");
-$footer=file_get_contents("footer.html");
-echo $header.$banner.$footer;
+include("header.html");
+include("content.html");
+include("footer.html");
 ?>
