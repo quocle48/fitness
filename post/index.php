3da4bf78a4c44b40145dfabe675f1cd83f9ff1be
xuanbac216 <xuanbac216@gmail.com>
2016-06-15 15:08:24 +0700

    no message

--- post/index.php
+++ post/index.php
@@ -3,17 +3,16 @@
 <div class="container">
 	<div class="row ">
 		<div class="col-xs-12 col-md-8 ">
-		phần bên trái
+		
 		<?php
 			for($i=0;$i<4;$i++)
 				include("thum-post.html");
 		?>
 		</div>
 		<div class="col-xs-6 col-md-4">
-		phần bên phải
 		<?php
 			include("categories.html");
-			include("categories.html");
+			include("statistical.html");
 		?>
 		</div>
 	</div>
