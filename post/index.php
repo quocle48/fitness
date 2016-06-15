418def6dee52e5644a1976b1ae2f568a12d53a13
Thai Trung Duc <dbom.1006@gmail.com>
2016-06-15 21:15:17 +0700

    ádasd

--- post/index.php
+++ post/index.php
@@ -1,28 +1,24 @@
 <?php
 	include("../header.html"); 
 	include("banner.html"); ?>
-<div class="container">
-	<div class="row ">
-		<div class="col-xs-12 col-md-8 ">
-		
-		<?php
-			$t=1;
-			if($t==1){
-				for($i=0;$i<4;$i++)
-				include("thum-post.html");
-			}
-			if($t==2){
-				include("post-detail-1.html");
-			}
-		?>
-		</div>
-		<div class="col-xs-6 col-md-4">
-		<?php
-			include("categories.html");
-			include("statistical.html");
-		?>
-		</div>
+<div class="container post">
+	<div class="col-xs-12 col-md-8 ">
+	<?php
+		$t=1;
+		if($t==1){
+			for($i=0;$i<4;$i++)
+			include("thum-post.html");
+		}
+		if($t==2){
+			include("post-detail-1.html");
+		}
+	?>
 	</div>
-	
+	<div class="col-xs-6 col-md-4">
+	<?php
+		include("categories.html");
+		include("statistical.html");
+	?>
+	</div>
 </div>
 <?php include("../footer.html"); ?>
