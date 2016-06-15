462c4f96d28035c63bf248e8b15b2a3a45c43aee
xuanbac216 <xuanbac216@gmail.com>
2016-06-15 12:48:20 +0700

    no message

--- post/index.php
+++ post/index.php
@@ -1,17 +1,22 @@
 <?php include("header.html"); ?>
 <?php include("banner.html"); ?>
 <div class="container">
-	<div class="col-xs-12 col-sm-12 col-md-8 col-lg-8">
+	<div class="row ">
+		<div class="col-xs-12 col-md-8 ">
+		phần bên trái
 		<?php
-			for($i=0;$i<5;$i++)
+			for($i=0;$i<4;$i++)
 				include("thum-post.html");
 		?>
-	</div>
-	<div class="col-xs-0 col-sm-0 col-md-4 col-lg-4">
+		</div>
+		<div class="col-xs-6 col-md-4">
+		phần bên phải
 		<?php
 			include("categories.html");
 			include("categories.html");
 		?>
+		</div>
 	</div>
+	
 </div>
 <?php include("../footer.html"); ?>
