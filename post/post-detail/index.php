832cc2809e4c274fa5486493d9ad0240941f3e22
xuanbac216 <xuanbac216@gmail.com>
2016-06-15 20:12:14 +0700

    no message

--- post/post-detail/index.php
+++ post/post-detail/index.php
@@ -1,18 +1,12 @@
-<?php include("header.html"); ?>
-<?php include("banner.html"); ?>
+<?php include("../header.html"); ?>
+<?php include("../banner.html"); ?>
 <div class="container">
 	<div class="row ">
 		<div class="col-xs-12 col-md-8 ">
 		
 		<?php
-			$t=1;
-			if($t==1){
-				for($i=0;$i<4;$i++)
+			for($i=0;$i<4;$i++)
 				include("thum-post.html");
-			}
-			if($t==2){
-				include("post-detail-1.html");
-			}
 		?>
 		</div>
 		<div class="col-xs-6 col-md-4">
@@ -24,4 +18,4 @@
 	</div>
 	
 </div>
-<?php include("../footer.html"); ?>
+<?php include("../../footer.html"); ?>
