80b6ce0c4ae8520d3df27085ecaf8edec17e2469
Thai Trung Duc <dbom.1006@gmail.com>
2016-06-15 10:29:17 +0700

    demo - post

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
 include("header.html");
-include("banner.html"); 
+include("slider.html"); 
 include("content.html");
 include("footer.html");
 ?>
---
+++ post/index.php
@@ -0,0 +1,17 @@
+<?php include("header.html"); ?>
+<?php include("banner.html"); ?>
+<div class="container">
+	<div class="col-xs-12 col-sm-12 col-md-8 col-lg-8">
+		<?php
+			for(int i=0;i<5;i++)
+				include("thum-post.html");
+		?>
+	</div>
+	<div class="col-xs-0 col-sm-0 col-md-4 col-lg-4">
+		<?php
+			include("categories.html");
+			include("categories.html");
+		?>
+	</div>
+</div>
+<?php include("footer.html"); ?>
