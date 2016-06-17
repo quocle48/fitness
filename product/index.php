24905e5085e4592aae5ceff559e79080caf681e0
Thai Trung Duc <dbom.1006@gmail.com>
2016-06-18 01:26:48 +0700

    no message

--- product/index.php
+++ product/index.php
@@ -2,25 +2,18 @@
 	include("../header.html"); 
 	include("../banner.html"); ?>
 <div class="container post">
-	<div class="col-xs-12 col-md-8 ">
 	<?php
 		$list=!isset($_GET["id"]);
-		if($list){
-			for($i=0;$i<10;$i++)
-				include("thum-post.html");
-			include("page-number.html");
-		}
+		if(!$list)	include("product-detail.html");
 		else{
-			include("product-detai.html");
+			for($i=0;$i<10;$i++){
+	?>
+	<div class="list-items col-xs-12 col-sm-6 col-md-6 col-lg-3">
+	<?php include("product.html");?>
+	</div>
+	<?php		
+			}
 		}
 	?>
-	</div>
-	<div class="col-xs-6 col-md-4">
-	<?php
-		include("../categories.html");
-		include("../practice.html");
-		include("../statistical.html");
-	?>
-	</div>
 </div>
 <?php include("../footer.html"); ?>
