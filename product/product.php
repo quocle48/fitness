018dc93c9127e3d0e3c602f5d8827e94c6b40517
Thai Trung Duc <dbom.1006@gmail.com>
2016-06-22 22:47:20 +0700

    update

--- product/product.php
+++ product/product.php
@@ -1,7 +1,7 @@
 <?php 
 	Class Product{
-		$id = "";
-		$title = ""; //Thuộc tính
+		var $id;
+		var $title; //Thuộc tính
 		
 
 		function Product($id,$title){ //Hàm tạo
@@ -15,4 +15,9 @@
 	}
 
 	//Nếu có phân loại hay kế thừa thì viết thêm dưới ni
+
+
+	//Ví dụ về cách sử dụng
+	$pro1=new Product(1,"Giày Undermour");
+	echo $pro1->getId();
 ?>
