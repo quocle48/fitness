f3233de456dcb1c133d32df567e0c2e957b00fa7
Thai Trung Duc <dbom.1006@gmail.com>
2016-06-22 23:18:54 +0700

    fix

--- application/user/login.php
+++ application/user/login.php
@@ -1,5 +1,5 @@
 <?php
-	session_start();
+    session_start();
     include("../../header.html"); 
     include("../../banner.html");
     if(isset($_POST['log-click'])){
@@ -12,21 +12,23 @@
         if(strlen($_POST["pass"])>=6 && !empty($_POST["pass"]))
 			$pass=$_POST["pass"];
         else $Err=true;
-        if($Err) echo "<div class='notice'>Thông tin tài khoản khởi tạo chưa chính xác. Xin vui lòng thử lại.";
+        if($Err) echo "<div class='notice'>Thông tin tài khoản đăng nhập chưa chính xác. Xin vui lòng thử lại.";
         else{
             $conn=connectDb();
             $sql="select password from user where username='".$user."' ";
             $result= $conn->query($sql);
             if( password_verify($pass, $result->fetchColumn())){
-				$_SESSION["username"]=$user;
+				$_SESSION['username']=$user;
+                echo "<div class='notice'> Đăng nhập thành công. ";
 			}
-                
             else{
                 echo "<div class='notice'> Đăng nhập thất bại, Xin vui lòng kiểm tra lại mật khẩu! ";
             }
             disconnectDb($conn);
-            
-        } 
+        }
+        echo '<a href = "javascript:history.back()" style="font-size: 20px;display:block;margin-top: 10px">Back to previous page (auto after 5s)</a>';
+        echo '</div>';
+        echo "<script>window.setTimeout('history.back()',5000);</script>";
        
     }
     else{
