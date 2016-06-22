3b77575cf687cbee657b3abb0f744966838cdbf8
Thai Trung Duc <dbom.1006@gmail.com>
2016-06-22 14:07:46 +0700

    database user mới

--- database/fitness.sql
+++ database/fitness.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 12, 2016 at 06:43 PM
+-- Generation Time: Jun 22, 2016 at 08:57 AM
 -- Server version: 10.1.9-MariaDB
 -- PHP Version: 5.6.15
 
@@ -19,6 +19,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `fitness`
 --
+CREATE DATABASE IF NOT EXISTS `fitness` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+USE `fitness`;
 
 -- --------------------------------------------------------
 
@@ -27,21 +29,19 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `user` (
-  `name` tinytext CHARACTER SET latin1 NOT NULL,
-  `username` varchar(50) CHARACTER SET latin1 NOT NULL,
-  `password` varchar(255) CHARACTER SET latin1 NOT NULL,
-  `email` varchar(255) CHARACTER SET latin1 NOT NULL,
-  `created` date NOT NULL,
-  `id_user` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `id` int(11) NOT NULL,
+  `username` varchar(55) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(55) COLLATE utf8_unicode_ci NOT NULL,
+  `level` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`name`, `username`, `password`, `email`, `created`, `id_user`) VALUES
-('Duc', 'dbom', '123456', 'dbom.1006@gmail.com', '2016-06-06', 1),
-('Quoc', 'quoclee', '123456', 'quoclee48@gmail.com', '2016-06-08', 3);
+INSERT INTO `user` (`id`, `username`, `password`, `email`, `level`) VALUES
+(1, 'dbom', '123123', 'dbom@gmail.com', 0);
 
 --
 -- Indexes for dumped tables
@@ -52,7 +52,8 @@ INSERT INTO `user` (`name`, `username`, `password`, `email`, `created`, `id_user
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`username`),
-  ADD UNIQUE KEY `id_user` (`id_user`);
+  ADD UNIQUE KEY `email` (`email`),
+  ADD KEY `id` (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -62,8 +63,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-ufitnessserfitness
