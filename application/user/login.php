f10b514bd5378118751300023a8f0c93f1097c24
Thai Trung Duc <dbom.1006@gmail.com>
2016-06-25 11:29:58 +0700

    fix

--- application/user/login.php
+++ application/user/login.php
@@ -1,36 +1,44 @@
 <?php
-    include("../../header.html"); 
-    include("../../banner.html");
+    include_once("../libraries/config.php");
     if(isset($_POST['log-click'])){
-        $Err=false;
+        $Err="";
         $user = $mail = $pass = "";
         if (strlen($_POST["user"])>=4 && !empty($_POST["user"]))
             $user = $_POST["user"];
-        else $Err=true;
-       
+        else $Err="Tài khoản";
         if(strlen($_POST["pass"])>=6 && !empty($_POST["pass"]))
 			$pass=$_POST["pass"];
-        else $Err=true;
-        if($Err) echo "<div class='notice'>Thông tin tài khoản đăng nhập chưa chính xác. Xin vui lòng thử lại.";
-        else{
+        else $Err="Mật khẩu";
+        if($Err=="")
+        {
             $conn=connectDb();
-            $sql="select password from user where username='".$user."' ";
-            $result= $conn->query($sql);
-            if( password_verify($pass, $result->fetchColumn())){
-				$_SESSION['username']=$user;
-                echo "<div class='notice'> Đăng nhập thành công. ";
-			}
-            else{
-                echo "<div class='notice'> Đăng nhập thất bại, Xin vui lòng kiểm tra lại mật khẩu! ";
+            $stmt = $conn->prepare("SELECT password,level FROM user where username='".$user."' "); 
+            $stmt->execute();
+            $result = $stmt->fetchAll();
+            if(count($result)>0){
+                $account=$result[0];
+                $p=$account['password'];
+                if(password_verify($pass, $p)){
+    				$_SESSION['username']=$user;
+                    $_SESSION['level']=$account['level'];
+    			}
+                else $Err="Mật khẩu";  
             }
+            else $Err="Tài khoản"; 
             disconnectDb($conn);
         }
-        echo '<a href = "javascript:history.back()" style="font-size: 20px;display:block;margin-top: 10px">Back to previous page (auto after 3s)</a>';
-        echo '</div>';
-        echo "<script>window.setTimeout('history.back()',3000);</script>";
+        include("../../header.html"); 
+        include("../../banner.html");
+        if($Err!="") echo "<div class='notice'>".$Err." không chính xác. Xin vui lòng thử lại! ";
+        else echo "<div class='notice'> Đăng nhập thành công. ";
+        // echo '<a href = "javascript:history.back()" style="font-size: 20px;display:block;margin-top: 10px">Back to previous page (auto after 3s)</a>';
+         echo '</div>';
+        // echo "<script>window.setTimeout('history.back()',3000);</script>";
        
     }
     else{
+        include("../../header.html"); 
+        include("../../banner.html");
         echo "<div class='notice'>Có lỗi trong quá trình tạo tài khoản. Xin vui lòng thử lại.";
         echo '<a href = "javascript:history.back()" style="font-size: 20px;display:block;margin-top: 10px">Back to previous page (auto after 3s)</a>';
         echo '</div>';
--- application/user/logout.php
+++ application/user/logout.php
@@ -1,8 +1,8 @@
 <?php
-include("../../header.html"); 
-include("../../banner.html");
 session_destroy();
 session_unset();
+include("../../header.html"); 
+include("../../banner.html");
 echo "<div class='notice'>Đăng xuất thành công";
 echo '<a href = "javascript:history.back()" style="font-size: 20px;display:block;margin-top: 10px">Back to previous page (auto after 3s)</a>';
 echo '</div>';
