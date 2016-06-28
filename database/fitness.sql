dd97941d6e36a3217d3a4caa1771acf3223b80fc
Quoc Le <quoclee48@gmail.com>
2016-06-28 11:27:40 +0700

    update database

--- database/fitness.sql
+++ database/fitness.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
+-- version 4.2.11
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 22, 2016 at 08:57 AM
--- Server version: 10.1.9-MariaDB
--- PHP Version: 5.6.15
+-- Generation Time: Jun 28, 2016 at 06:22 AM
+-- Server version: 5.6.21
+-- PHP Version: 5.6.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,13 +14,137 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `fitness`
 --
+DROP DATABASE `fitness`;
 CREATE DATABASE IF NOT EXISTS `fitness` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 USE `fitness`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `function`
+--
+
+DROP TABLE IF EXISTS `function`;
+CREATE TABLE IF NOT EXISTS `function` (
+`id_function` int(11) NOT NULL,
+  `name_function` varchar(55) COLLATE utf8_unicode_ci NOT NULL,
+  `description` text COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `group`
+--
+
+DROP TABLE IF EXISTS `group`;
+CREATE TABLE IF NOT EXISTS `group` (
+`id_group` int(11) NOT NULL,
+  `name_group` varchar(55) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `group`
+--
+
+INSERT INTO `group` (`id_group`, `name_group`) VALUES
+(1, 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `group_function`
+--
+
+DROP TABLE IF EXISTS `group_function`;
+CREATE TABLE IF NOT EXISTS `group_function` (
+  `id_group` int(11) NOT NULL,
+  `id_function` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `post`
+--
+
+DROP TABLE IF EXISTS `post`;
+CREATE TABLE IF NOT EXISTS `post` (
+`id_post` int(11) NOT NULL,
+  `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `tag` text COLLATE utf8_unicode_ci NOT NULL,
+  `content` text COLLATE utf8_unicode_ci NOT NULL,
+  `time` datetime NOT NULL,
+  `lesson` varchar(55) COLLATE utf8_unicode_ci NOT NULL,
+  `type` varchar(55) COLLATE utf8_unicode_ci NOT NULL,
+  `like` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `post_comment`
+--
+
+DROP TABLE IF EXISTS `post_comment`;
+CREATE TABLE IF NOT EXISTS `post_comment` (
+  `id_cmt` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_post` int(11) NOT NULL,
+  `content` text COLLATE utf8_unicode_ci NOT NULL,
+  `like` int(11) NOT NULL,
+  `super_cmt` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+DROP TABLE IF EXISTS `product`;
+CREATE TABLE IF NOT EXISTS `product` (
+`id_product` int(11) NOT NULL,
+  `name_product` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `id_type` int(11) NOT NULL,
+  `brand` varchar(55) COLLATE utf8_unicode_ci NOT NULL,
+  `content` text COLLATE utf8_unicode_ci NOT NULL,
+  `tag` text COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_type`
+--
+
+DROP TABLE IF EXISTS `product_type`;
+CREATE TABLE IF NOT EXISTS `product_type` (
+`id_type` int(11) NOT NULL,
+  `name_type` varchar(55) COLLATE utf8_unicode_ci NOT NULL,
+  `properties_name` varchar(55) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `store`
+--
+
+DROP TABLE IF EXISTS `store`;
+CREATE TABLE IF NOT EXISTS `store` (
+`id_product` int(11) NOT NULL,
+  `properties_value` varchar(55) COLLATE utf8_unicode_ci NOT NULL,
+  `price` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `sale` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -28,42 +152,182 @@ USE `fitness`;
 -- Table structure for table `user`
 --
 
-CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+`id_user` int(11) NOT NULL,
   `username` varchar(55) COLLATE utf8_unicode_ci NOT NULL,
   `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `email` varchar(55) COLLATE utf8_unicode_ci NOT NULL,
   `level` int(11) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `username`, `password`, `email`, `level`) VALUES
+INSERT INTO `user` (`id_user`, `username`, `password`, `email`, `level`) VALUES
 (1, 'dbom', '123123', 'dbom@gmail.com', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_group`
+--
+
+DROP TABLE IF EXISTS `user_group`;
+CREATE TABLE IF NOT EXISTS `user_group` (
+  `id_user` int(11) NOT NULL,
+  `id_group` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `function`
+--
+ALTER TABLE `function`
+ ADD PRIMARY KEY (`id_function`);
+
+--
+-- Indexes for table `group`
+--
+ALTER TABLE `group`
+ ADD PRIMARY KEY (`id_group`);
+
+--
+-- Indexes for table `group_function`
+--
+ALTER TABLE `group_function`
+ ADD PRIMARY KEY (`id_group`,`id_function`), ADD KEY `group_function_ibfk_2` (`id_function`);
+
+--
+-- Indexes for table `post`
+--
+ALTER TABLE `post`
+ ADD PRIMARY KEY (`id_post`), ADD KEY `FK_post_user` (`id_user`);
+
+--
+-- Indexes for table `post_comment`
+--
+ALTER TABLE `post_comment`
+ ADD PRIMARY KEY (`id_cmt`), ADD KEY `FK_post_comment_user` (`id_user`), ADD KEY `FK_post_comment_post` (`id_post`), ADD KEY `FK_post_comment_post_comment` (`super_cmt`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+ ADD PRIMARY KEY (`id_product`), ADD KEY `FK_product_product_type` (`id_type`);
+
+--
+-- Indexes for table `product_type`
+--
+ALTER TABLE `product_type`
+ ADD PRIMARY KEY (`id_type`);
+
+--
+-- Indexes for table `store`
+--
+ALTER TABLE `store`
+ ADD PRIMARY KEY (`id_product`);
+
+--
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`username`),
-  ADD UNIQUE KEY `email` (`email`),
-  ADD KEY `id` (`id`);
+ ADD PRIMARY KEY (`username`), ADD UNIQUE KEY `email` (`email`), ADD KEY `id` (`id_user`);
+
+--
+-- Indexes for table `user_group`
+--
+ALTER TABLE `user_group`
+ ADD PRIMARY KEY (`id_user`,`id_group`), ADD KEY `user_group_ibfk_2` (`id_group`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `function`
+--
+ALTER TABLE `function`
+MODIFY `id_function` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `group`
+--
+ALTER TABLE `group`
+MODIFY `id_group` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `post`
+--
+ALTER TABLE `post`
+MODIFY `id_post` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+MODIFY `id_product` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `product_type`
+--
+ALTER TABLE `product_type`
+MODIFY `id_type` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `store`
+--
+ALTER TABLE `store`
+MODIFY `id_product` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `group_function`
+--
+ALTER TABLE `group_function`
+ADD CONSTRAINT `group_function_ibfk_1` FOREIGN KEY (`id_group`) REFERENCES `group` (`id_group`),
+ADD CONSTRAINT `group_function_ibfk_2` FOREIGN KEY (`id_function`) REFERENCES `function` (`id_function`);
+
+--
+-- Constraints for table `post`
+--
+ALTER TABLE `post`
+ADD CONSTRAINT `FK_post_user` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`);
+
+--
+-- Constraints for table `post_comment`
+--
+ALTER TABLE `post_comment`
+ADD CONSTRAINT `FK_post_comment_post` FOREIGN KEY (`id_post`) REFERENCES `post` (`id_post`),
+ADD CONSTRAINT `FK_post_comment_post_comment` FOREIGN KEY (`super_cmt`) REFERENCES `post_comment` (`id_cmt`),
+ADD CONSTRAINT `FK_post_comment_user` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`);
+
+--
+-- Constraints for table `product`
+--
+ALTER TABLE `product`
+ADD CONSTRAINT `FK_product_product_type` FOREIGN KEY (`id_type`) REFERENCES `product_type` (`id_type`),
+ADD CONSTRAINT `FK_product_store` FOREIGN KEY (`id_product`) REFERENCES `store` (`id_product`);
+
+--
+-- Constraints for table `store`
+--
+ALTER TABLE `store`
+ADD CONSTRAINT `FK_store_product` FOREIGN KEY (`id_product`) REFERENCES `product` (`id_product`);
+
+--
+-- Constraints for table `user_group`
+--
+ALTER TABLE `user_group`
+ADD CONSTRAINT `user_group_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`),
+ADD CONSTRAINT `user_group_ibfk_2` FOREIGN KEY (`id_group`) REFERENCES `group` (`id_group`);
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
