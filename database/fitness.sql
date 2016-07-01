40551affb4f9e572873839abd731ba5abb87e33f
Thai Trung Duc <dbom.1006@gmail.com>
2016-07-01 15:55:17 +0700

    IMPORT DATABASE

--- database/fitness.sql
+++ database/fitness.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.11
+-- version 4.5.1
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 28, 2016 at 06:22 AM
--- Server version: 5.6.21
--- PHP Version: 5.6.3
+-- Generation Time: Jul 01, 2016 at 10:53 AM
+-- Server version: 10.1.9-MariaDB
+-- PHP Version: 5.6.15
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `fitness`
@@ -30,11 +30,18 @@ USE `fitness`;
 --
 
 DROP TABLE IF EXISTS `function`;
-CREATE TABLE IF NOT EXISTS `function` (
-`id_function` int(11) NOT NULL,
-  `name_function` varchar(55) COLLATE utf8_unicode_ci NOT NULL,
+CREATE TABLE `function` (
+  `id` int(11) NOT NULL,
+  `name` varchar(55) COLLATE utf8_unicode_ci NOT NULL,
   `description` text COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `function`
+--
+
+INSERT INTO `function` (`id`, `name`, `description`) VALUES
+(4, 'gád', 'ádasd');
 
 -- --------------------------------------------------------
 
@@ -43,17 +50,19 @@ CREATE TABLE IF NOT EXISTS `function` (
 --
 
 DROP TABLE IF EXISTS `group`;
-CREATE TABLE IF NOT EXISTS `group` (
-`id_group` int(11) NOT NULL,
-  `name_group` varchar(55) COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+CREATE TABLE `group` (
+  `id` int(11) NOT NULL,
+  `name` varchar(55) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `group`
 --
 
-INSERT INTO `group` (`id_group`, `name_group`) VALUES
-(1, 'admin');
+INSERT INTO `group` (`id`, `name`) VALUES
+(1, 'admin'),
+(2, 'vip'),
+(3, 'seller');
 
 -- --------------------------------------------------------
 
@@ -62,9 +71,9 @@ INSERT INTO `group` (`id_group`, `name_group`) VALUES
 --
 
 DROP TABLE IF EXISTS `group_function`;
-CREATE TABLE IF NOT EXISTS `group_function` (
-  `id_group` int(11) NOT NULL,
-  `id_function` int(11) NOT NULL
+CREATE TABLE `group_function` (
+  `group_id` int(11) NOT NULL,
+  `function_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
@@ -74,10 +83,10 @@ CREATE TABLE IF NOT EXISTS `group_function` (
 --
 
 DROP TABLE IF EXISTS `post`;
-CREATE TABLE IF NOT EXISTS `post` (
-`id_post` int(11) NOT NULL,
+CREATE TABLE `post` (
+  `id` int(11) NOT NULL,
   `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `id_user` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
   `tag` text COLLATE utf8_unicode_ci NOT NULL,
   `content` text COLLATE utf8_unicode_ci NOT NULL,
   `time` datetime NOT NULL,
@@ -93,7 +102,7 @@ CREATE TABLE IF NOT EXISTS `post` (
 --
 
 DROP TABLE IF EXISTS `post_comment`;
-CREATE TABLE IF NOT EXISTS `post_comment` (
+CREATE TABLE `post_comment` (
   `id_cmt` int(11) NOT NULL,
   `id_user` int(11) NOT NULL,
   `id_post` int(11) NOT NULL,
@@ -109,26 +118,13 @@ CREATE TABLE IF NOT EXISTS `post_comment` (
 --
 
 DROP TABLE IF EXISTS `product`;
-CREATE TABLE IF NOT EXISTS `product` (
-`id_product` int(11) NOT NULL,
-  `name_product` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `id_type` int(11) NOT NULL,
+CREATE TABLE `product` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `type_id` int(11) NOT NULL,
   `brand` varchar(55) COLLATE utf8_unicode_ci NOT NULL,
   `content` text COLLATE utf8_unicode_ci NOT NULL,
   `tag` text COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `product_type`
---
-
-DROP TABLE IF EXISTS `product_type`;
-CREATE TABLE IF NOT EXISTS `product_type` (
-`id_type` int(11) NOT NULL,
-  `name_type` varchar(55) COLLATE utf8_unicode_ci NOT NULL,
-  `properties_name` varchar(55) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
@@ -138,8 +134,8 @@ CREATE TABLE IF NOT EXISTS `product_type` (
 --
 
 DROP TABLE IF EXISTS `store`;
-CREATE TABLE IF NOT EXISTS `store` (
-`id_product` int(11) NOT NULL,
+CREATE TABLE `store` (
+  `id_product` int(11) NOT NULL,
   `properties_value` varchar(55) COLLATE utf8_unicode_ci NOT NULL,
   `price` int(11) NOT NULL,
   `quantity` int(11) NOT NULL,
@@ -149,24 +145,39 @@ CREATE TABLE IF NOT EXISTS `store` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `type_product`
+--
+
+DROP TABLE IF EXISTS `type_product`;
+CREATE TABLE `type_product` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `description` text COLLATE utf8_unicode_ci NOT NULL,
+  `properties_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `user`
 --
 
 DROP TABLE IF EXISTS `user`;
-CREATE TABLE IF NOT EXISTS `user` (
-`id_user` int(11) NOT NULL,
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
   `username` varchar(55) COLLATE utf8_unicode_ci NOT NULL,
   `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `email` varchar(55) COLLATE utf8_unicode_ci NOT NULL,
   `level` int(11) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id_user`, `username`, `password`, `email`, `level`) VALUES
-(1, 'dbom', '123123', 'dbom@gmail.com', 0);
+INSERT INTO `user` (`id`, `username`, `password`, `email`, `level`) VALUES
+(8, '12311asd', '$2y$10$xIdB6t4/2nqp9o64rSWjSOJ.EWDgb4huXY8FBNZ4.LYtcVAAv4fK2', '123123123@gmail.com', 0),
+(2, 'dbom', '$2y$10$MOy4vCJ8wnQ2V6Pqhie99.f8mAuMSgD9ApZoIIfJjVfkrdITyylIy', 'dbom123@gmail.com', 1);
 
 -- --------------------------------------------------------
 
@@ -175,10 +186,20 @@ INSERT INTO `user` (`id_user`, `username`, `password`, `email`, `level`) VALUES
 --
 
 DROP TABLE IF EXISTS `user_group`;
-CREATE TABLE IF NOT EXISTS `user_group` (
-  `id_user` int(11) NOT NULL,
-  `id_group` int(11) NOT NULL
+CREATE TABLE `user_group` (
+  `user_id` int(11) NOT NULL,
+  `group_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `user_group`
+--
+
+INSERT INTO `user_group` (`user_id`, `group_id`) VALUES
+(2, 1),
+(2, 3),
+(8, 2),
+(8, 3);
 
 --
 -- Indexes for dumped tables
@@ -188,61 +209,71 @@ CREATE TABLE IF NOT EXISTS `user_group` (
 -- Indexes for table `function`
 --
 ALTER TABLE `function`
- ADD PRIMARY KEY (`id_function`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `group`
 --
 ALTER TABLE `group`
- ADD PRIMARY KEY (`id_group`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `group_function`
 --
 ALTER TABLE `group_function`
- ADD PRIMARY KEY (`id_group`,`id_function`), ADD KEY `group_function_ibfk_2` (`id_function`);
+  ADD PRIMARY KEY (`group_id`,`function_id`),
+  ADD KEY `group_function_ibfk_2` (`function_id`);
 
 --
 -- Indexes for table `post`
 --
 ALTER TABLE `post`
- ADD PRIMARY KEY (`id_post`), ADD KEY `FK_post_user` (`id_user`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_post_user` (`user_id`);
 
 --
 -- Indexes for table `post_comment`
 --
 ALTER TABLE `post_comment`
- ADD PRIMARY KEY (`id_cmt`), ADD KEY `FK_post_comment_user` (`id_user`), ADD KEY `FK_post_comment_post` (`id_post`), ADD KEY `FK_post_comment_post_comment` (`super_cmt`);
+  ADD PRIMARY KEY (`id_cmt`),
+  ADD KEY `FK_post_comment_user` (`id_user`),
+  ADD KEY `FK_post_comment_post` (`id_post`),
+  ADD KEY `FK_post_comment_post_comment` (`super_cmt`);
 
 --
 -- Indexes for table `product`
 --
 ALTER TABLE `product`
- ADD PRIMARY KEY (`id_product`), ADD KEY `FK_product_product_type` (`id_type`);
-
---
--- Indexes for table `product_type`
---
-ALTER TABLE `product_type`
- ADD PRIMARY KEY (`id_type`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_product_product_type` (`type_id`);
 
 --
 -- Indexes for table `store`
 --
 ALTER TABLE `store`
- ADD PRIMARY KEY (`id_product`);
+  ADD PRIMARY KEY (`id_product`);
+
+--
+-- Indexes for table `type_product`
+--
+ALTER TABLE `type_product`
+  ADD PRIMARY KEY (`name`),
+  ADD KEY `id` (`id`);
 
 --
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
- ADD PRIMARY KEY (`username`), ADD UNIQUE KEY `email` (`email`), ADD KEY `id` (`id_user`);
+  ADD PRIMARY KEY (`username`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD KEY `id` (`id`);
 
 --
 -- Indexes for table `user_group`
 --
 ALTER TABLE `user_group`
- ADD PRIMARY KEY (`id_user`,`id_group`), ADD KEY `user_group_ibfk_2` (`id_group`);
+  ADD PRIMARY KEY (`user_id`,`group_id`),
+  ADD KEY `user_group_ibfk_2` (`group_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -252,37 +283,37 @@ ALTER TABLE `user_group`
 -- AUTO_INCREMENT for table `function`
 --
 ALTER TABLE `function`
-MODIFY `id_function` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `group`
 --
 ALTER TABLE `group`
-MODIFY `id_group` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `post`
 --
 ALTER TABLE `post`
-MODIFY `id_post` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-MODIFY `id_product` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `product_type`
---
-ALTER TABLE `product_type`
-MODIFY `id_type` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `store`
 --
 ALTER TABLE `store`
-MODIFY `id_product` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_product` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `type_product`
+--
+ALTER TABLE `type_product`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 --
 -- Constraints for dumped tables
 --
@@ -291,42 +322,42 @@ MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 -- Constraints for table `group_function`
 --
 ALTER TABLE `group_function`
-ADD CONSTRAINT `group_function_ibfk_1` FOREIGN KEY (`id_group`) REFERENCES `group` (`id_group`),
-ADD CONSTRAINT `group_function_ibfk_2` FOREIGN KEY (`id_function`) REFERENCES `function` (`id_function`);
+  ADD CONSTRAINT `group_function_ibfk_1` FOREIGN KEY (`group_id`) REFERENCES `group` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `group_function_ibfk_2` FOREIGN KEY (`function_id`) REFERENCES `function` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `post`
 --
 ALTER TABLE `post`
-ADD CONSTRAINT `FK_post_user` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`);
+  ADD CONSTRAINT `FK_post_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
 
 --
 -- Constraints for table `post_comment`
 --
 ALTER TABLE `post_comment`
-ADD CONSTRAINT `FK_post_comment_post` FOREIGN KEY (`id_post`) REFERENCES `post` (`id_post`),
-ADD CONSTRAINT `FK_post_comment_post_comment` FOREIGN KEY (`super_cmt`) REFERENCES `post_comment` (`id_cmt`),
-ADD CONSTRAINT `FK_post_comment_user` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`);
+  ADD CONSTRAINT `FK_post_comment_post` FOREIGN KEY (`id_post`) REFERENCES `post` (`id`),
+  ADD CONSTRAINT `FK_post_comment_post_comment` FOREIGN KEY (`super_cmt`) REFERENCES `post_comment` (`id_cmt`),
+  ADD CONSTRAINT `FK_post_comment_user` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`);
 
 --
 -- Constraints for table `product`
 --
 ALTER TABLE `product`
-ADD CONSTRAINT `FK_product_product_type` FOREIGN KEY (`id_type`) REFERENCES `product_type` (`id_type`),
-ADD CONSTRAINT `FK_product_store` FOREIGN KEY (`id_product`) REFERENCES `store` (`id_product`);
+  ADD CONSTRAINT `FK_product_product_type` FOREIGN KEY (`type_id`) REFERENCES `product_type` (`id_type`),
+  ADD CONSTRAINT `FK_product_store` FOREIGN KEY (`id`) REFERENCES `store` (`id_product`);
 
 --
 -- Constraints for table `store`
 --
 ALTER TABLE `store`
-ADD CONSTRAINT `FK_store_product` FOREIGN KEY (`id_product`) REFERENCES `product` (`id_product`);
+  ADD CONSTRAINT `FK_store_product` FOREIGN KEY (`id_product`) REFERENCES `product` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `user_group`
 --
 ALTER TABLE `user_group`
-ADD CONSTRAINT `user_group_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`),
-ADD CONSTRAINT `user_group_ibfk_2` FOREIGN KEY (`id_group`) REFERENCES `group` (`id_group`);
+  ADD CONSTRAINT `user_group_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `user_group_ibfk_2` FOREIGN KEY (`group_id`) REFERENCES `group` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
