ca329aff8ddc8885ec97bf1cf82515cb710f048d
Thai Trung Duc <dbom.1006@gmail.com>
2016-07-01 21:15:20 +0700

    new database

--- database/fitness.sql
+++ database/fitness.sql
@@ -1,97 +1,98 @@
--- phpMyAdmin SQL Dump
--- version 4.2.11
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Jun 28, 2016 at 06:22 AM
--- Server version: 5.6.21
--- PHP Version: 5.6.3
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               10.1.9-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win32
+-- HeidiSQL Version:             9.3.0.4984
+-- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
---
--- Database: `fitness`
---
-DROP DATABASE `fitness`;
-CREATE DATABASE IF NOT EXISTS `fitness` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+-- Dumping database structure for fitness
+DROP DATABASE IF EXISTS `fitness`;
+CREATE DATABASE IF NOT EXISTS `fitness` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
 USE `fitness`;
 
--- --------------------------------------------------------
 
---
--- Table structure for table `function`
---
-
+-- Dumping structure for table fitness.function
 DROP TABLE IF EXISTS `function`;
 CREATE TABLE IF NOT EXISTS `function` (
-`id_function` int(11) NOT NULL,
-  `name_function` varchar(55) COLLATE utf8_unicode_ci NOT NULL,
-  `description` text COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(55) COLLATE utf8_unicode_ci NOT NULL,
+  `description` text COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- --------------------------------------------------------
+-- Dumping data for table fitness.function: ~1 rows (approximately)
+DELETE FROM `function`;
+/*!40000 ALTER TABLE `function` DISABLE KEYS */;
+INSERT INTO `function` (`id`, `name`, `description`) VALUES
+	(4, 'gád', 'ádasd');
+/*!40000 ALTER TABLE `function` ENABLE KEYS */;
 
---
--- Table structure for table `group`
---
 
+-- Dumping structure for table fitness.group
 DROP TABLE IF EXISTS `group`;
 CREATE TABLE IF NOT EXISTS `group` (
-`id_group` int(11) NOT NULL,
-  `name_group` varchar(55) COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(55) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
---
--- Dumping data for table `group`
---
+-- Dumping data for table fitness.group: ~4 rows (approximately)
+DELETE FROM `group`;
+/*!40000 ALTER TABLE `group` DISABLE KEYS */;
+INSERT INTO `group` (`id`, `name`) VALUES
+	(1, 'admin'),
+	(2, 'vip'),
+	(3, 'seller'),
+	(4, 'poster');
+/*!40000 ALTER TABLE `group` ENABLE KEYS */;
 
-INSERT INTO `group` (`id_group`, `name_group`) VALUES
-(1, 'admin');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `group_function`
---
-
+-- Dumping structure for table fitness.group_function
 DROP TABLE IF EXISTS `group_function`;
 CREATE TABLE IF NOT EXISTS `group_function` (
-  `id_group` int(11) NOT NULL,
-  `id_function` int(11) NOT NULL
+  `group_id` int(11) NOT NULL,
+  `function_id` int(11) NOT NULL,
+  PRIMARY KEY (`group_id`,`function_id`),
+  KEY `group_function_ibfk_2` (`function_id`),
+  CONSTRAINT `group_function_ibfk_1` FOREIGN KEY (`group_id`) REFERENCES `group` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `group_function_ibfk_2` FOREIGN KEY (`function_id`) REFERENCES `function` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- --------------------------------------------------------
+-- Dumping data for table fitness.group_function: ~0 rows (approximately)
+DELETE FROM `group_function`;
+/*!40000 ALTER TABLE `group_function` DISABLE KEYS */;
+/*!40000 ALTER TABLE `group_function` ENABLE KEYS */;
 
---
--- Table structure for table `post`
---
 
+-- Dumping structure for table fitness.post
 DROP TABLE IF EXISTS `post`;
 CREATE TABLE IF NOT EXISTS `post` (
-`id_post` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `id_user` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
   `tag` text COLLATE utf8_unicode_ci NOT NULL,
   `content` text COLLATE utf8_unicode_ci NOT NULL,
   `time` datetime NOT NULL,
   `lesson` varchar(55) COLLATE utf8_unicode_ci NOT NULL,
   `type` varchar(55) COLLATE utf8_unicode_ci NOT NULL,
-  `like` int(11) NOT NULL
+  `like` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_post_user` (`user_id`),
+  CONSTRAINT `FK_post_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- --------------------------------------------------------
+-- Dumping data for table fitness.post: ~0 rows (approximately)
+DELETE FROM `post`;
+/*!40000 ALTER TABLE `post` DISABLE KEYS */;
+/*!40000 ALTER TABLE `post` ENABLE KEYS */;
 
---
--- Table structure for table `post_comment`
---
 
+-- Dumping structure for table fitness.post_comment
 DROP TABLE IF EXISTS `post_comment`;
 CREATE TABLE IF NOT EXISTS `post_comment` (
   `id_cmt` int(11) NOT NULL,
@@ -99,235 +100,118 @@ CREATE TABLE IF NOT EXISTS `post_comment` (
   `id_post` int(11) NOT NULL,
   `content` text COLLATE utf8_unicode_ci NOT NULL,
   `like` int(11) NOT NULL,
-  `super_cmt` int(11) NOT NULL
+  `super_cmt` int(11) NOT NULL,
+  PRIMARY KEY (`id_cmt`),
+  KEY `FK_post_comment_user` (`id_user`),
+  KEY `FK_post_comment_post` (`id_post`),
+  KEY `FK_post_comment_post_comment` (`super_cmt`),
+  CONSTRAINT `FK_post_comment_post` FOREIGN KEY (`id_post`) REFERENCES `post` (`id`),
+  CONSTRAINT `FK_post_comment_post_comment` FOREIGN KEY (`super_cmt`) REFERENCES `post_comment` (`id_cmt`),
+  CONSTRAINT `FK_post_comment_user` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- --------------------------------------------------------
+-- Dumping data for table fitness.post_comment: ~0 rows (approximately)
+DELETE FROM `post_comment`;
+/*!40000 ALTER TABLE `post_comment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `post_comment` ENABLE KEYS */;
 
---
--- Table structure for table `product`
---
 
+-- Dumping structure for table fitness.product
 DROP TABLE IF EXISTS `product`;
 CREATE TABLE IF NOT EXISTS `product` (
-`id_product` int(11) NOT NULL,
-  `name_product` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `id_type` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `type_id` int(11) NOT NULL,
   `brand` varchar(55) COLLATE utf8_unicode_ci NOT NULL,
   `content` text COLLATE utf8_unicode_ci NOT NULL,
-  `tag` text COLLATE utf8_unicode_ci NOT NULL
+  `tag` text COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_product_product_type` (`type_id`),
+  CONSTRAINT `FK_product_product_type` FOREIGN KEY (`type_id`) REFERENCES `product_type` (`id_type`),
+  CONSTRAINT `FK_product_store` FOREIGN KEY (`id`) REFERENCES `store` (`id_product`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- --------------------------------------------------------
+-- Dumping data for table fitness.product: ~0 rows (approximately)
+DELETE FROM `product`;
+/*!40000 ALTER TABLE `product` DISABLE KEYS */;
+/*!40000 ALTER TABLE `product` ENABLE KEYS */;
 
---
--- Table structure for table `product_type`
---
 
-DROP TABLE IF EXISTS `product_type`;
-CREATE TABLE IF NOT EXISTS `product_type` (
-`id_type` int(11) NOT NULL,
-  `name_type` varchar(55) COLLATE utf8_unicode_ci NOT NULL,
-  `properties_name` varchar(55) COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `store`
---
-
+-- Dumping structure for table fitness.store
 DROP TABLE IF EXISTS `store`;
 CREATE TABLE IF NOT EXISTS `store` (
-`id_product` int(11) NOT NULL,
+  `id_product` int(11) NOT NULL AUTO_INCREMENT,
   `properties_value` varchar(55) COLLATE utf8_unicode_ci NOT NULL,
   `price` int(11) NOT NULL,
   `quantity` int(11) NOT NULL,
-  `sale` int(11) NOT NULL
+  `sale` int(11) NOT NULL,
+  PRIMARY KEY (`id_product`),
+  CONSTRAINT `FK_store_product` FOREIGN KEY (`id_product`) REFERENCES `product` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- --------------------------------------------------------
+-- Dumping data for table fitness.store: ~0 rows (approximately)
+DELETE FROM `store`;
+/*!40000 ALTER TABLE `store` DISABLE KEYS */;
+/*!40000 ALTER TABLE `store` ENABLE KEYS */;
 
---
--- Table structure for table `user`
---
 
+-- Dumping structure for table fitness.type_product
+DROP TABLE IF EXISTS `type_product`;
+CREATE TABLE IF NOT EXISTS `type_product` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `description` text COLLATE utf8_unicode_ci NOT NULL,
+  `properties_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`name`),
+  KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- Dumping data for table fitness.type_product: ~0 rows (approximately)
+DELETE FROM `type_product`;
+/*!40000 ALTER TABLE `type_product` DISABLE KEYS */;
+/*!40000 ALTER TABLE `type_product` ENABLE KEYS */;
+
+
+-- Dumping structure for table fitness.user
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
-`id_user` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(55) COLLATE utf8_unicode_ci NOT NULL,
   `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `email` varchar(55) COLLATE utf8_unicode_ci NOT NULL,
-  `level` int(11) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `level` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`username`),
+  UNIQUE KEY `email` (`email`),
+  KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
---
--- Dumping data for table `user`
---
+-- Dumping data for table fitness.user: ~1 rows (approximately)
+DELETE FROM `user`;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` (`id`, `username`, `password`, `email`, `level`) VALUES
+	(2, 'dbom', '$2y$10$MOy4vCJ8wnQ2V6Pqhie99.f8mAuMSgD9ApZoIIfJjVfkrdITyylIy', 'dbom123@gmail.com', 1);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
 
-INSERT INTO `user` (`id_user`, `username`, `password`, `email`, `level`) VALUES
-(1, 'dbom', '123123', 'dbom@gmail.com', 0);
 
--- --------------------------------------------------------
-
---
--- Table structure for table `user_group`
---
-
+-- Dumping structure for table fitness.user_group
 DROP TABLE IF EXISTS `user_group`;
 CREATE TABLE IF NOT EXISTS `user_group` (
-  `id_user` int(11) NOT NULL,
-  `id_group` int(11) NOT NULL
+  `user_id` int(11) NOT NULL,
+  `group_id` int(11) NOT NULL,
+  PRIMARY KEY (`user_id`,`group_id`),
+  KEY `user_group_ibfk_2` (`group_id`),
+  CONSTRAINT `user_group_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `user_group_ibfk_2` FOREIGN KEY (`group_id`) REFERENCES `group` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `function`
---
-ALTER TABLE `function`
- ADD PRIMARY KEY (`id_function`);
-
---
--- Indexes for table `group`
---
-ALTER TABLE `group`
- ADD PRIMARY KEY (`id_group`);
-
---
--- Indexes for table `group_function`
---
-ALTER TABLE `group_function`
- ADD PRIMARY KEY (`id_group`,`id_function`), ADD KEY `group_function_ibfk_2` (`id_function`);
-
---
--- Indexes for table `post`
---
-ALTER TABLE `post`
- ADD PRIMARY KEY (`id_post`), ADD KEY `FK_post_user` (`id_user`);
-
---
--- Indexes for table `post_comment`
---
-ALTER TABLE `post_comment`
- ADD PRIMARY KEY (`id_cmt`), ADD KEY `FK_post_comment_user` (`id_user`), ADD KEY `FK_post_comment_post` (`id_post`), ADD KEY `FK_post_comment_post_comment` (`super_cmt`);
-
---
--- Indexes for table `product`
---
-ALTER TABLE `product`
- ADD PRIMARY KEY (`id_product`), ADD KEY `FK_product_product_type` (`id_type`);
-
---
--- Indexes for table `product_type`
---
-ALTER TABLE `product_type`
- ADD PRIMARY KEY (`id_type`);
-
---
--- Indexes for table `store`
---
-ALTER TABLE `store`
- ADD PRIMARY KEY (`id_product`);
-
---
--- Indexes for table `user`
---
-ALTER TABLE `user`
- ADD PRIMARY KEY (`username`), ADD UNIQUE KEY `email` (`email`), ADD KEY `id` (`id_user`);
-
---
--- Indexes for table `user_group`
---
-ALTER TABLE `user_group`
- ADD PRIMARY KEY (`id_user`,`id_group`), ADD KEY `user_group_ibfk_2` (`id_group`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `function`
---
-ALTER TABLE `function`
-MODIFY `id_function` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `group`
---
-ALTER TABLE `group`
-MODIFY `id_group` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT for table `post`
---
-ALTER TABLE `post`
-MODIFY `id_post` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `product`
---
-ALTER TABLE `product`
-MODIFY `id_product` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `product_type`
---
-ALTER TABLE `product_type`
-MODIFY `id_type` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `store`
---
-ALTER TABLE `store`
-MODIFY `id_product` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `user`
---
-ALTER TABLE `user`
-MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `group_function`
---
-ALTER TABLE `group_function`
-ADD CONSTRAINT `group_function_ibfk_1` FOREIGN KEY (`id_group`) REFERENCES `group` (`id_group`),
-ADD CONSTRAINT `group_function_ibfk_2` FOREIGN KEY (`id_function`) REFERENCES `function` (`id_function`);
-
---
--- Constraints for table `post`
---
-ALTER TABLE `post`
-ADD CONSTRAINT `FK_post_user` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`);
-
---
--- Constraints for table `post_comment`
---
-ALTER TABLE `post_comment`
-ADD CONSTRAINT `FK_post_comment_post` FOREIGN KEY (`id_post`) REFERENCES `post` (`id_post`),
-ADD CONSTRAINT `FK_post_comment_post_comment` FOREIGN KEY (`super_cmt`) REFERENCES `post_comment` (`id_cmt`),
-ADD CONSTRAINT `FK_post_comment_user` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`);
-
---
--- Constraints for table `product`
---
-ALTER TABLE `product`
-ADD CONSTRAINT `FK_product_product_type` FOREIGN KEY (`id_type`) REFERENCES `product_type` (`id_type`),
-ADD CONSTRAINT `FK_product_store` FOREIGN KEY (`id_product`) REFERENCES `store` (`id_product`);
-
---
--- Constraints for table `store`
---
-ALTER TABLE `store`
-ADD CONSTRAINT `FK_store_product` FOREIGN KEY (`id_product`) REFERENCES `product` (`id_product`);
-
---
--- Constraints for table `user_group`
---
-ALTER TABLE `user_group`
-ADD CONSTRAINT `user_group_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`),
-ADD CONSTRAINT `user_group_ibfk_2` FOREIGN KEY (`id_group`) REFERENCES `group` (`id_group`);
-
+-- Dumping data for table fitness.user_group: ~3 rows (approximately)
+DELETE FROM `user_group`;
+/*!40000 ALTER TABLE `user_group` DISABLE KEYS */;
+INSERT INTO `user_group` (`user_id`, `group_id`) VALUES
+	(2, 1),
+	(2, 3),
+	(2, 4);
+/*!40000 ALTER TABLE `user_group` ENABLE KEYS */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
