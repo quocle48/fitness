741fc803a10eb281ebcd6197dd280162889588c2
xuanbac216 <xuanbac216@gmail.com>
2016-07-14 21:40:19 +0700

    show post list and post detail,

--- post/index.php
+++ post/index.php
@@ -8,23 +8,46 @@
 <div class="container post">
 	<div class="col-xs-12 col-md-9 ">
 	<?php
-		
-		
-		$list= (isset( $_GET['page'] ) ) ? $_GET['page'] : 1;
-		$listPost = new pagination("select a.id, a.title, a.like , a.time, d.username from post a, level_post b, category_post c , user d where a.user_id=d.id and b.id = a.level_id and c.id = a.category_id","10");
-		$result = $listPost->getData($list);
+		if(isset( $_GET['level'])){
+			$level = (isset( $_GET['level'] ) ) ? $_GET['level'] : 1;	
+			$list= (isset( $_GET['page'] ) ) ? $_GET['page'] : 1;
+			$sql = "select a.id, a.title, a.like , a.time, a.level_id, d.username from post a, level_post b, category_post c , user d where a.user_id=d.id and b.id = a.level_id and c.id = a.category_id and a.level_id=".$level;
+			$listPost = new pagination($sql,"5");
+			$result = $listPost->getData($list);
 
-		if($result->rowCount()>0)
-    	{
-            while($row=$result->fetch(PDO::FETCH_ASSOC))
-            {
-            	include("thum-post.html");
-            }
-     
-	    }
-	    else
-	        echo "<div class='notice'>No data!</div>";
-		
+			if($result->rowCount()>0)
+	    	{
+	            while($row=$result->fetch(PDO::FETCH_ASSOC))
+	            {
+	            	include("thum-post.html");
+	            }
+		    }
+		    else
+		        echo "<div class='notice'>No data!</div>";
+			 
+			$listPages = $listPost->listPages();
+			echo $listPages;	
+			$listPost->closeConn();
+		}
+		if(isset($_GET['id'])){
+			$id = $_GET['id'];
+			$conn=connectDb();
+			$conn->exec("set names utf8");
+			$sql = "select a.id, a.title, a.like , a.time, a.level_id, d.username from post a, level_post b, category_post c , user d where a.user_id=d.id and b.id = a.level_id and c.id = a.category_id and a.id=".$id;
+			$query = $conn->prepare($sql);
+			$query->execute();
+			if($query->rowCount()>0)
+	    	{
+	            while($row=$query->fetch(PDO::FETCH_ASSOC))
+	            {
+	            	include("post-detail.html");
+	            }
+		    }
+		    else
+		        echo "<div class='notice'>No find data!</div>";
+		    disconnectDb($conn);
+			
+		}
 	?>
 	</div>
 	<div class="col-xs-6 col-md-3">
